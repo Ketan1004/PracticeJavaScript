efabd2cb8d7003d3118982389fc0122975e6471c
Ashish Shrikhande <89147823+Ketan1004@users.noreply.github.com>
2024-02-14 07:45:57 +0000

    understood

--- Basics/scope.js
+++ Basics/scope.js
@@ -1,10 +1,26 @@
-let a = 10;
-var b = 20;
-var c = 30;
- if (true){
-    let a = 100
-    const  b =200
-    var c = 300;
-    console.log("Inner: " ,a )
- }
-  console.log(a);
+// let a = 10;
+// var b = 20;
+// var c = 30;
+//  if (true){
+//     let a = 100
+//     const  b =200
+//     var c = 300;
+//     console.log("Inner: " ,a )
+//  }
+//   console.log(a);
+
+  //++++++++++++++++++++++++ Interesting +++++++++++++++++++++
+  console.log(addOne(5));
+   function addOne(num){
+    return num + 1;
+   }
+    
+   addTwo(5)
+    const addTwo =  function (num){
+        return num + 2;
+    }
+
+
+    
+
+   
